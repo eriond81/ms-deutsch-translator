94d39f66cf6ae54fc0c75e058e92cdbc23b3bc32
Pablo Ull <pablo.ull.salcedo@gmail.com>
2022-01-18 23:10:31 +0100

    subida inicial
    plantilla

---
+++ src/main/java/com/translator/deutsch/MsDeutschTranslatorApplication.java
@@ -0,0 +1,13 @@
+package com.translator.deutsch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsDeutschTranslatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsDeutschTranslatorApplication.class, args);
+	}
+
+}
